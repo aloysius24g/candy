fd5e43bf9df298f494203eb8e8a05654246220f4
8 <>
2025-11-14 11:31:32 +0530

    formatted

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-	base: '/candy/', // for github pages.
+  base: '/candy/', // for github pages.
   plugins: [react(), tailwindcss()],
 });
